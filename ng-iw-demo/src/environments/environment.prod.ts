96dc6d96fcf9e2dfdf5eac4bb3f0a38a7bebb96d
Pablo A. Luna <46759965+pabloaluna@users.noreply.github.com>
2021-11-01 17:13:18 -0300

    fixes

--- ng-iw-demo/src/environments/environment.prod.ts
+++ ng-iw-demo/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  IW_FRONTEND_URL: "https://identity-wallet.apps-qa.extrimian.com",
+  DAPP_API_URL: "http://localhost:5000/api",  
 };
